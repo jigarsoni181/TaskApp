86789deefa1ff0942718570fc08d42d4c69e0bbc
jigarsoni181 <jigarsoni181@gmail.com>
2024-04-19 17:57:10 +0530

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,40 @@
 import logo from './logo.svg';
 import './App.css';
+import axios from 'axios';
+import {useState} from 'react'
 
-function App() {
+ function App() {
+
+  const [todos, setTodos] = useState([])
+
+  const getData = () => {
+    fetch('https://jsonplaceholder.typicode.com/todos')
+    .then((response) => response.json())
+    .then((data) => {
+      // console.log("Data==>", JSON.stringify(data));
+      setTodos(data)
+    }).catch((err) => {
+      console.log(err)
+    })
+  }
+
+  
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Task Application</h1>
+      <button onClick={getData} style={{marginBottom: "50px"}}>Click me for Task List</button>
+        <table style={{width: "600px", margin: "auto", border: "1px solid black", borderCollapse:"collapse"}}>
+        {todos.map((todo) => {
+          return(
+            
+              <tr key={todo.id}>
+                <td style={{border: "1px solid black", borderCollapse:"collapse"}}>{todo.title}</td>
+              </tr>
+            
+          )
+        })
+        }
+        </table>
     </div>
   );
 }
